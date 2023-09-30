32548b72c3c96cffa9c5493541a5585cac180a2d
Romanyerom <Romanyerom@github.com>
2023-09-29 23:31:49 -0700

    comment

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 	int swapped;
 
 	if (array == NULL || size <= 1)
-        return; // No sorting needed for NULL or small arrays
+        return; /* No sorting needed for NULL or small arrays */
 
 	for (i = 0; i < size - 1; i++)
 	{
